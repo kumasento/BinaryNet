bd3c701570543cceeed382eea814aad173ac3436
itayhubara <itayhubara@yahoo.com>
2016-03-16 08:59:53 +0200

    Added files via upload

--- Models/BinarySpatialConvolution.lua
+++ Models/BinarySpatialConvolution.lua
@@ -1,6 +1,6 @@
 local BinarySpatialConvolution, parent = torch.class('BinarySpatialConvolution', 'nn.Module')
 
-function BinarySpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW,padH)
+function BinarySpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
    parent.__init(self)
 
    dW = dW or 1
@@ -13,9 +13,8 @@ function BinarySpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, 
 
    self.dW = dW
    self.dH = dH
-   self.padding = padding or 0
    self.padW = padW or 0
-   self.padH = padH or 0
+   self.padH = padH or self.padW
 
    self.weight = torch.Tensor(nOutputPlane, nInputPlane, kH, kW)
    self.weightB = torch.Tensor(nOutputPlane, nInputPlane, kH, kW)
@@ -30,46 +29,56 @@ function BinarySpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, 
 end
 
 function BinarySpatialConvolution:reset(stdv)
-   if stdv then
-      stdv = stdv * math.sqrt(3)
-   else
-      stdv = 1/math.sqrt(self.kW*self.kH*self.nInputPlane)
-   end
-   if nn.oldSeed then
-      self.weight:apply(function()
-         return torch.uniform(-1, 1)
-      end)
-      self.bias:apply(function()
-         return torch.uniform(-1, 1)
-      end)
-   else
-      self.weight:uniform(-1,1)
-      self.bias:uniform(-1, 1)
-   end
-end
-
- function BinarySpatialConvolution:binarized(trainFlag)
-  self.weightOrg:copy(self.weight);
-  if not trainFlag and  self.stcWeights then
-    self.weightB:copy(weightOrg);
+  if stdv then
+     stdv = stdv * math.sqrt(3)
   else
-    self.weightB:copy(self.weight):add(1):div(2):clamp(0,1)
-
-    if not stcWeights then
-      self.weightB:round():mul(2):add(-1)
-    else
-      self.maskStc=self.weightB-self.randmat:rand(self.randmat:size())
-      self.weightB:copy(self.maskStc:sign())
-    end
+     stdv = 1/math.sqrt(self.kW*self.kH*self.nInputPlane)
   end
-
-  return  self.weightB
+  if nn.oldSeed then
+     self.weight:apply(function()
+        return torch.uniform(-1, 1)
+     end)
+     if self.bias then
+        self.bias:apply(function()
+        return torch.uniform(-stdv, stdv)
+        end)
+     end
+  else
+     self.weight:uniform(-1, 1)
+     if self.bias then
+        self.bias:uniform(-stdv, stdv)
+     end
   end
+end
+function BinarySpatialConvolution:binarized(trainFlag)
+ self.weightOrg:copy(self.weight);
+ if not trainFlag and  self.stcWeights then
+   self.weightB:copy(weightOrg);
+ else
+   self.weightB:copy(self.weight):add(1):div(2):clamp(0,1)
+
+   if not stcWeights then
+     self.weightB:round():mul(2):add(-1)
+   else
+     self.maskStc=self.weightB-self.randmat:rand(self.randmat:size())
+     self.weightB:copy(self.maskStc:sign())
+   end
+ end
+
+ return  self.weightB
+ end
 
 local function backCompatibility(self)
    self.finput = self.finput or self.weight.new()
    self.fgradInput = self.fgradInput or self.weight.new()
-   self.padding = self.padding or 0
+   if self.padding then
+      self.padW = self.padding
+      self.padH = self.padding
+      self.padding = nil
+   else
+      self.padW = self.padW or 0
+      self.padH = self.padH or 0
+   end
    if self.weight:dim() == 2 then
       self.weight = self.weight:view(self.nOutputPlane, self.nInputPlane, self.kH, self.kW)
    end
@@ -110,16 +119,25 @@ local function unviewWeight(self)
 end
 
 function BinarySpatialConvolution:updateOutput(input)
-  --print('In u')
    backCompatibility(self)
    viewWeight(self)
    input = makeContiguous(self, input)
    self.weightB = self:binarized(self.train)
    self.weight:copy(self.weightB)
-   local out = input.nn.SpatialConvolutionMM_updateOutput(self, input)
+   input.THNN.SpatialConvolutionMM_updateOutput(
+      input:cdata(),
+      self.output:cdata(),
+      self.weight:cdata(),
+      self.bias:cdata(),
+      self.finput:cdata(),
+      self.fgradInput:cdata(),
+      self.kW, self.kH,
+      self.dW, self.dH,
+      self.padW, self.padH
+   )
    self.weight:copy(self.weightOrg)
    unviewWeight(self)
-   return out
+   return self.output
 end
 
 function BinarySpatialConvolution:updateGradInput(input, gradOutput)
@@ -127,27 +145,64 @@ function BinarySpatialConvolution:updateGradInput(input, gradOutput)
       backCompatibility(self)
       viewWeight(self)
       input, gradOutput = makeContiguous(self, input, gradOutput)
-
       self.weight:copy(self.weightB)
-      local out = input.nn.SpatialConvolutionMM_updateGradInput(self, input, gradOutput)
+      input.THNN.SpatialConvolutionMM_updateGradInput(
+         input:cdata(),
+         gradOutput:cdata(),
+         self.gradInput:cdata(),
+         self.weight:cdata(),
+         self.bias:cdata(),
+         self.finput:cdata(),
+         self.fgradInput:cdata(),
+         self.kW, self.kH,
+         self.dW, self.dH,
+         self.padW, self.padH
+      )
       self.weight:copy(self.weightOrg)
-
       unviewWeight(self)
-      return out
+      return self.gradInput
    end
 end
 
 function BinarySpatialConvolution:accGradParameters(input, gradOutput, scale)
-   backCompatibility(self)
-   input, gradOutput = makeContiguous(self, input, gradOutput)
-   viewWeight(self)
-   local out = input.nn.SpatialConvolutionMM_accGradParameters(self, input, gradOutput, scale)
-   unviewWeight(self)
-   return out
+  scale = scale or 1
+  backCompatibility(self)
+  input, gradOutput = makeContiguous(self, input, gradOutput)
+  viewWeight(self)
+  input.THNN.SpatialConvolutionMM_accGradParameters(
+     input:cdata(),
+     gradOutput:cdata(),
+     self.gradWeight:cdata(),
+     self.gradBias:cdata(),
+     self.finput:cdata(),
+     self.fgradInput:cdata(),
+     self.kW, self.kH,
+     self.dW, self.dH,
+     self.padW, self.padH,
+     scale
+  )
+  unviewWeight(self)
 end
 
-function BinarySpatialConvolution:type(type)
-   self.finput = torch.Tensor()
-   self.fgradInput = torch.Tensor()
-   return parent.type(self,type)
+function BinarySpatialConvolution:type(type,tensorCache)
+   self.finput = self.finput and torch.Tensor()
+   self.fgradInput = self.fgradInput and torch.Tensor()
+   return parent.type(self,type,tensorCache)
+end
+
+function BinarySpatialConvolution:__tostring__()
+   local s = string.format('%s(%d -> %d, %dx%d', torch.type(self),
+         self.nInputPlane, self.nOutputPlane, self.kW, self.kH)
+   if self.dW ~= 1 or self.dH ~= 1 or self.padW ~= 0 or self.padH ~= 0 then
+     s = s .. string.format(', %d,%d', self.dW, self.dH)
+   end
+   if (self.padW or self.padH) and (self.padW ~= 0 or self.padH ~= 0) then
+     s = s .. ', ' .. self.padW .. ',' .. self.padH
+   end
+   return s .. ')'
+end
+
+function BinarySpatialConvolution:clearState()
+   nn.utils.clear(self, 'finput', 'fgradInput', '_input', '_gradOutput')
+   return parent.clearState(self)
 end
